0dc6ba9bfbc49201bf5bbbe5ce1cf5b3b2975c46
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-05-02 19:49:35 -0300

    university studies

---
+++ person.py
@@ -0,0 +1,5 @@
+nome = "Eduardo Goncalves Prestes" 
+idade = 23
+email = "eduardo@gmail.com"
+
+print("nome: " + nome, " idade:", idade,  "- O email cadastrado foi:", email)
---
+++ swap.py
@@ -0,0 +1,12 @@
+a = 5
+b = 1
+c = a
+
+print("variavel a antes de ter novo valor atribuido", a)
+print("variavel b", b)
+a = 6
+print("variavel c", c)
+print("variavel a depois de ter novo valor atribuido", a)
+a = "texto"
+print("variavel c depois de ter novo valor em string atribuido em a", c)
+print("variavel a depois de ter novo valor em string atribuido", a)
